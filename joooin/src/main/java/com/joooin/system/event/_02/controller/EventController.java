f08b67007fa5c9ef77f3439e10a89b94676ac910
李信廣 <allen@gmail.com>
2019-03-06 09:38:40 +0800

    Add controller test

---
+++ joooin/src/main/java/com/joooin/system/event/_02/controller/EventController.java
@@ -0,0 +1,20 @@
+package com.joooin.system.event._02.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class EventController {
+	
+	@RequestMapping("/event/{eventId}")
+	public String event(Model model, @PathVariable("eventId") String eventId, HttpServletRequest request) {
+		
+		return "event";
+	}
+	
+}
