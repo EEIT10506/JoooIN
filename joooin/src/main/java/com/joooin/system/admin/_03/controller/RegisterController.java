c1f145bfc3a219e528238747074204d6a8419940
王耀德 <felix1472y@gmail.com>
2019-03-06 11:37:52 +0800

    register 90% 差 memberimage

--- joooin/src/main/java/com/joooin/system/admin/_03/controller/RegisterController.java
+++ joooin/src/main/java/com/joooin/system/admin/_03/controller/RegisterController.java
@@ -1,5 +1,9 @@
 package com.joooin.system.admin._03.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -7,9 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.joooin.model.MemberMainBean;
+import com.joooin.system.admin._03.service.RegisterService;
 
 @Controller
 public class RegisterController {
+	
+	@Autowired
+	RegisterService service;
 	
 	@RequestMapping(value = "/register", method = RequestMethod.GET)
 	public String Registerpage(Model model) {
@@ -20,7 +28,17 @@ public class RegisterController {
 	
 	@RequestMapping(value = "/registerProcess", method = RequestMethod.POST)
 	public String Register(@ModelAttribute("memberMainBean")MemberMainBean mmb){
+		if(mmb!=null) {
+			System.out.println("no null");
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd ahh:mm:ss");
+			mmb.setMemberCreateDate(sdf.format(new Date()).toString());
+			mmb.setMemberImage(new Byte[] {1});
+			service.save(mmb);
+		}else {
+			System.out.println("null");
+		}
 		
-		return "/admin/register";
+		
+		return "redirect:/register";
 	}
 }
