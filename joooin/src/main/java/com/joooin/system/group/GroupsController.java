d1c9f6152e78aaaab31e5026f194682d83d8e0c0
陳昭樺 <kh88403@gmail.com>
2019-03-08 13:53:34 +0800

    增加社團主頁
    20190308
    增加groups.jsp
    增加grouptype.css
    增加GroupsController.java
    增加mag, create的icon_1

---
+++ joooin/src/main/java/com/joooin/system/group/GroupsController.java
@@ -0,0 +1,14 @@
+package com.joooin.system.group;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class GroupsController {
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/groups")
+	public String homepage() {
+		return "group/groups";
+	}
+}
