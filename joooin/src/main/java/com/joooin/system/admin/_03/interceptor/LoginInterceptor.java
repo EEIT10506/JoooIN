b6c5f0ccbce839b272e12386bf418b854d9e30f7
王耀德 <felix1472y@gmail.com>
2019-03-11 10:04:20 +0800

    add login interceptor

---
+++ joooin/src/main/java/com/joooin/system/admin/_03/interceptor/LoginInterceptor.java
@@ -0,0 +1,40 @@
+package com.joooin.system.admin._03.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.joooin.model.MemberMainBean;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		HttpSession session = request.getSession();
+		MemberMainBean mmb = (MemberMainBean) session.getAttribute("memberName");
+		if (mmb != null) {
+			return true;
+		} else {
+			response.sendRedirect(request.getContextPath() + "/admin/login");
+			return false;
+		}
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+
+	}
+
+}
