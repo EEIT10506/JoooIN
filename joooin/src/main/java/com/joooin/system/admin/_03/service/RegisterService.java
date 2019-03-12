b4a919c49b722dcf18fead3dc5c5f53f80bea791
王耀德 <felix1472y@gmail.com>
2019-03-13 00:32:51 +0800

    信箱認證OK

--- joooin/src/main/java/com/joooin/system/admin/_03/service/RegisterService.java
+++ joooin/src/main/java/com/joooin/system/admin/_03/service/RegisterService.java
@@ -2,6 +2,9 @@ package com.joooin.system.admin._03.service;
 
 import java.util.List;
 
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
+
 import com.joooin.model.MemberMainBean;
 
 public interface RegisterService {
@@ -13,5 +16,7 @@ public interface RegisterService {
 	public void deleteByMemberId(Integer memberId);
 	public Boolean checkEmail(String email);
 	public Boolean activeUser(String certificationHash);
-	
+	public void certification(MemberMainBean mmb, HttpServletRequest request)throws MessagingException;
+	public MemberMainBean checkCertification(String email);
+	public void certificationChangeStatus(String certificationHash);
 }
