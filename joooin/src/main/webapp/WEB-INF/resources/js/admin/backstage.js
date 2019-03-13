3c3393ba7a1c0870c4c43c8683bcc03ebf5a72c4
王耀德 <felix1472y@gmail.com>
2019-03-13 14:18:06 +0800

    admin.jsp大改 backstage.js 新增撈所有會員資料ajax及切換按鍵方法

---
+++ joooin/src/main/webapp/WEB-INF/resources/js/admin/backstage.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+
+    $('.list-group-item').click(selectCategory);
+    		
+    		
+    	function selectCategory() {
+
+        $('.list-group-item').removeClass('bg-primary').removeClass('text-white').addClass('text-primary');
+        $(this).removeClass('text-primary').addClass('bg-primary').addClass('text-white');
+        $('#content').empty();
+
+        var title = $(this).text();
+        $('#content').prepend($('<h2></h2>').text(title));
+        $('#content> p').remove();
+        
+        // 以上為切換後台按鈕功能
+        if (title == '會員管理'){
+        	$.ajax({
+        		type: "GET",
+                url: "/joooin/getAllMember",
+                dataType: "json",
+                timeout: 600000,
+                
+                success: function (data){
+                	var tab = $('<table></table>').addClass('table table-striped').html('<thead><tr><th class="text-center">會員編號</th><th>會員姓名</th><th>會員信箱</th><th>會員密碼</th><th>會員性別</th><th>會員電話</th><th>會員登入次數</th><th>會員加入日期</th><th>會員認證狀態</th></tr></thead><tbody></tbody>');
+                	$('#content>h2').after(tab);
+                		var docFrag = $(document.createDocumentFragment());
+                		console.log(data);
+                		$.each(data, function (idx,Member){
+                			var cell1 = $('<td></td>').text(Member.memberId)
+                			var cell2 = $('<td></td>').text(Member.memberName)
+                			var cell3 = $('<td></td>').text(Member.email)
+                			var cell4 = $('<td></td>').text(Member.password)
+                			var cell5 = $('<td></td>').text(Member.gender)
+                			var cell6 = $('<td></td>').text(Member.phone)
+                			var cell7 = $('<td></td>').text(Member.logins)
+                			var cell8 = $('<td></td>').text(Member.memberCreateDate)
+                			var cell9 = $('<td></td>').text(Member.certificationStatus)
+                			
+                			var row = $('<tr></tr>').append([cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9]);
+                			
+                			docFrag.append(row);
+                		})
+                		$('#content tbody').html(docFrag);
+                }//function (data)
+        	})	//ajax end;
+        }		//會員資料end;
+        
+    	}		//selectCategory end;
+    	
+    	
+})
+    	
