c4a021973eb2a6a1c5aae49ec6b11ed8fd565a22
ToolMan47 <kh88403@gmail.com>
2019-03-17 23:20:15 +0800

    準備ajax部分
    ProcessController增加@ResponseBody

--- joooin/src/main/java/com/joooin/system/group/_22/controller/ProcessGroupController.java
+++ joooin/src/main/java/com/joooin/system/group/_22/controller/ProcessGroupController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.joooin.model.MemberMainBean;
 import com.joooin.system.group._22.service.GroupService_22;
@@ -22,6 +23,7 @@ public class ProcessGroupController {
 	ServletContext context;
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/group/applygroup/{groupId}")
+	@ResponseBody
 	public String getGroupApplyList(@PathVariable Integer groupId, Model model) {
 		
 		List<MemberMainBean> groupApplyList = service.getGroupApplyMembers(groupId);
@@ -33,10 +35,11 @@ public class ProcessGroupController {
 
 	@RequestMapping(method = RequestMethod.POST, value =
 			"/group/applygroup/{groupId}/member/{memberId}/result/{decide}")
+	@ResponseBody
 	public String processGroupApply(@PathVariable Integer groupId, 
 			@PathVariable Integer memberId, @PathVariable String decide ) {
 		
-		
+		service.processApplyList(groupId, memberId, decide);
 		
 		
 		
