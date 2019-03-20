105f205e716ed59fb494912a14034b6996daa21c
ToolMan47 <kh88403@gmail.com>
2019-03-20 20:13:52 +0800

    新增社團首頁要用的pojo

--- joooin/src/main/java/com/joooin/system/group/_22/service/impl/GroupServiceImpl_22.java
+++ joooin/src/main/java/com/joooin/system/group/_22/service/impl/GroupServiceImpl_22.java
@@ -18,6 +18,7 @@ import com.joooin.repository.GroupMainDao;
 import com.joooin.repository.GroupMemberDao;
 import com.joooin.repository.GroupPostDao;
 import com.joooin.repository.MemberMainDao;
+import com.joooin.system.group._22.pojo.Poster;
 import com.joooin.system.group._22.service.GroupService_22;
 
 @Transactional
@@ -32,7 +33,7 @@ public class GroupServiceImpl_22 implements GroupService_22 {
 
 	@Autowired
 	GroupMemberDao groupMemberDao;
-	
+
 	@Autowired
 	GroupPostDao groupPostDao;
 
@@ -123,7 +124,7 @@ public class GroupServiceImpl_22 implements GroupService_22 {
 	}
 
 	@Override
-	//使用小心，這是複製人
+	// 使用小心，這是複製人
 	public List<GroupMemberBean> getProcessGroupApplyList(Integer groupId) {
 		List<GroupMemberBean> groupApplyList = new LinkedList<GroupMemberBean>();
 		List<GroupMemberBean> allGroupMember = groupMainDao.getByGroupId(groupId).getGroupMemberList();
@@ -143,27 +144,28 @@ public class GroupServiceImpl_22 implements GroupService_22 {
 	@Override
 	public Integer processApplyList(Integer groupId, Integer memberId, String decide) {
 		GroupMainBean groupMain = groupMainDao.getByGroupId(groupId);
-		
-		//version.1
-		for(GroupMemberBean realGMBean : groupMemberDao.getAll()) {
-			if(realGMBean.getGroupId().equals(groupId) && realGMBean.getMemberId().equals(memberId) && decide.equals("approve")) {
+
+		// version.1
+		for (GroupMemberBean realGMBean : groupMemberDao.getAll()) {
+			if (realGMBean.getGroupId().equals(groupId) && realGMBean.getMemberId().equals(memberId)
+					&& decide.equals("approve")) {
 
 				realGMBean.setIsAgreed(true);
 				groupMemberDao.update(realGMBean);
-				
+
 				groupMain.setGroupCurrentMembers(groupMain.getGroupCurrentMembers() + 1);
 				groupMainDao.update(groupMain);
-				
+
 				return realGMBean.getMemberId();
-			}
-			else if(realGMBean.getGroupId().equals(groupId) && realGMBean.getMemberId().equals(memberId) && decide.equals("reject")) {
+			} else if (realGMBean.getGroupId().equals(groupId) && realGMBean.getMemberId().equals(memberId)
+					&& decide.equals("reject")) {
 				groupMemberDao.deleteByGroupMemberId(realGMBean.getGroupMemberId());
 				break;
 			}
-			
+
 		}
 		return null;
-		
+
 //
 //		for (GroupMemberBean member : gmList) {
 //			if(member.getMemberId().equals(memberId) && decide.equals("approve")) {
@@ -176,7 +178,7 @@ public class GroupServiceImpl_22 implements GroupService_22 {
 //		}
 //		groupMain.setGroupMemberList(gmList);
 //		groupMainDao.update(groupMain);
-		
+
 //		List<GroupMemberBean> groupApplyList = getProcessGroupApplyList(groupId);
 //		for(GroupMemberBean gmBean : groupApplyList) {
 //			if(gmBean.getMemberId().equals(memberId) && decide.equals("approve")) {
@@ -230,7 +232,7 @@ public class GroupServiceImpl_22 implements GroupService_22 {
 //				System.out.println("服務失敗");
 //				return null;
 //			}
-		
+
 //			 else if(gmBean.getMemberId().equals(memberId) && decide.equals("reject")) {
 //				System.out.println("gmId:" + gmBean.getGroupMemberId());
 //				List<GroupMemberBean> allGM = groupMemberDao.getAll();
@@ -252,14 +254,13 @@ public class GroupServiceImpl_22 implements GroupService_22 {
 	}
 
 	@Override
-	//新版2019320
+	// 新版2019320
 	public List<MemberMainBean> getMembersInGroup(Integer groupId) {
 		List<MemberMainBean> memberInGroupList = new LinkedList<MemberMainBean>();
 		List<GroupMemberBean> allGroupMember = groupMainDao.getByGroupId(groupId).getGroupMemberList();
 
 		for (GroupMemberBean gmBean : allGroupMember) {
-			if (gmBean.getIsAgreed())
-			{	
+			if (gmBean.getIsAgreed()) {
 				MemberMainBean member = memMainDao.getByMemberId(gmBean.getMemberId());
 				memberInGroupList.add(member);
 			}
@@ -271,5 +272,29 @@ public class GroupServiceImpl_22 implements GroupService_22 {
 	@Override
 	public Integer createPost(GroupPostBean groupPostBean) {
 		return groupPostDao.save(groupPostBean);
+	}
+
+	@Override
+	public List<Poster> getPostersByGroupId (Integer groupId) {
+		List<GroupPostBean> groupPosts = groupPostDao.getAll();
+		List<Poster> postersByGroupId = new LinkedList<Poster>();
+		for(GroupPostBean post : groupPosts) {
+			if(post.getGroupId().equals(groupId)) {
+				Poster poster = new Poster();
+				poster.setGroupId(groupId);
+				poster.setGroupPostContent(post.getGroupPostContent());
+				poster.setGroupPostDate(post.getGroupPostDate());
+				poster.setGroupPostId(post.getGroupPostId());
+				poster.setGroupPostLike(post.getGroupPostLike());
+				poster.setGroupPostTitle(post.getGroupPostTitle());
+				poster.setIsDeleted(post.getIsDeleted());
+				poster.setMemberId(post.getMemberId());
+				String memberName = memMainDao.getByMemberId(post.getMemberId()).getMemberName();
+				poster.setMemberName(memberName);
+				
+				postersByGroupId.add(poster);
+			}
+		}
+		return postersByGroupId;
 	}
 }
--- joooin/src/main/webapp/WEB-INF/views/group/group.jsp
+++ joooin/src/main/webapp/WEB-INF/views/group/group.jsp
@@ -221,7 +221,7 @@ div {
 			<table id="table_id" class="display table table-striped table-sm">
 				<thead>
 					<tr>
-						<th>編號</th>
+						<th>讚數</th>
 						<th>標題</th>
 						<th>發文人</th>
 						<th>發文日期</th>
@@ -229,118 +229,15 @@ div {
 					</tr>
 				</thead>
 				<tbody>
+				<c:forEach var="poster" items="${groupPoster}" varStatus="loop">
 					<tr>
-						<td>1,001</td>
-						<td>Lorem</td>
-						<td>ipsum</td>
-						<td>dolor</td>
-						<td>sit</td>
+						<td>${poster.groupPostLike}</td>
+						<td>${poster.groupPostTitle }</td>
+						<td>${poster.memberName }</td>
+						<td>${poster.groupPostDate}</td>
+						<td>等待回覆</td>
 					</tr>
-					<tr>
-						<td>1,002</td>
-						<td>amet</td>
-						<td>consectetur</td>
-						<td>adipiscing</td>
-						<td>elit</td>
-					</tr>
-					<tr>
-						<td>1,003</td>
-						<td>Integer</td>
-						<td>nec</td>
-						<td>odio</td>
-						<td>Praesent</td>
-					</tr>
-					<tr>
-						<td>1,003</td>
-						<td>libero</td>
-						<td>Sed</td>
-						<td>cursus</td>
-						<td>ante</td>
-					</tr>
-					<tr>
-						<td>1,004</td>
-						<td>dapibus</td>
-						<td>diam</td>
-						<td>Sed</td>
-						<td>nisi</td>
-					</tr>
-					<tr>
-						<td>1,005</td>
-						<td>Nulla</td>
-						<td>quis</td>
-						<td>sem</td>
-						<td>at</td>
-					</tr>
-					<tr>
-						<td>1,006</td>
-						<td>nibh</td>
-						<td>elementum</td>
-						<td>imperdiet</td>
-						<td>Duis</td>
-					</tr>
-					<tr>
-						<td>1,007</td>
-						<td>sagittis</td>
-						<td>ipsum</td>
-						<td>Praesent</td>
-						<td>mauris</td>
-					</tr>
-					<tr>
-						<td>1,008</td>
-						<td>Fusce</td>
-						<td>nec</td>
-						<td>tellus</td>
-						<td>sed</td>
-					</tr>
-					<tr>
-						<td>1,009</td>
-						<td>augue</td>
-						<td>semper</td>
-						<td>porta</td>
-						<td>Mauris</td>
-					</tr>
-					<tr>
-						<td>1,010</td>
-						<td>massa</td>
-						<td>Vestibulum</td>
-						<td>lacinia</td>
-						<td>arcu</td>
-					</tr>
-					<tr>
-						<td>1,011</td>
-						<td>eget</td>
-						<td>nulla</td>
-						<td>Class</td>
-						<td>aptent</td>
-					</tr>
-					<tr>
-						<td>1,012</td>
-						<td>taciti</td>
-						<td>sociosqu</td>
-						<td>ad</td>
-						<td>litora</td>
-					</tr>
-					<tr>
-						<td>1,013</td>
-						<td>torquent</td>
-						<td>per</td>
-						<td>conubia</td>
-						<td>nostra</td>
-					</tr>
-					<tr>
-						<td>1,014</td>
-						<td>per</td>
-						<td>inceptos</td>
-						<td>himenaeos</td>
-						<td>Curabitur</td>
-					</tr>
-					<tr>
-						<td>1,015</td>
-						<td>sodales</td>
-						<td>ligula</td>
-						<td>in</td>
-						<td>libero</td>
-					</tr>
+				</c:forEach>
 				</tbody>
 			</table>
 		</div>
