9aeeb4b3bbcc6a939a77a2be24d1ae32771f4d31
ToolMan47 <kh88403@gmail.com>
2019-03-20 11:42:17 +0800

    精簡getMembersInGroup程式碼

--- joooin/src/main/java/com/joooin/system/group/_22/service/impl/GroupServiceImpl_22.java
+++ joooin/src/main/java/com/joooin/system/group/_22/service/impl/GroupServiceImpl_22.java
@@ -249,25 +249,16 @@ public class GroupServiceImpl_22 implements GroupService_22 {
 	@Override
 	public List<MemberMainBean> getMembersInGroup(Integer groupId) {
 		List<MemberMainBean> memberInGroupList = new LinkedList<MemberMainBean>();
-
-		List<GroupMemberBean> groupApplyList = new LinkedList<GroupMemberBean>();
 		List<GroupMemberBean> allGroupMember = groupMainDao.getByGroupId(groupId).getGroupMemberList();
 
 		for (GroupMemberBean gmBean : allGroupMember) {
-			//
 			if (gmBean.getIsAgreed())
-			//
-			{
-				groupApplyList.add(gmBean);
-			}
-		}
-		
-		for (GroupMemberBean gmBean : groupApplyList) {
-			if (gmBean.getIsAgreed()) {
+			{	
 				MemberMainBean member = memMainDao.getByMemberId(gmBean.getMemberId());
 				memberInGroupList.add(member);
 			}
 		}
+
 		return memberInGroupList;
 	}
 }
