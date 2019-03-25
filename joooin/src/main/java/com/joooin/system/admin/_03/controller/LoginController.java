15b97dd034e3f177afe8e5618b8ea92e6e1498fd
王耀德 <felix1472y@gmail.com>
2019-03-26 02:01:36 +0800

    新增簡易檢舉懲罰實作

--- joooin/src/main/java/com/joooin/system/admin/_03/controller/LoginController.java
+++ joooin/src/main/java/com/joooin/system/admin/_03/controller/LoginController.java
@@ -1,5 +1,10 @@
 package com.joooin.system.admin._03.controller;
 
+import java.text.ParseException;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -14,6 +19,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.joooin.model.AdminBean;
 import com.joooin.model.MemberMainBean;
+import com.joooin.model.PunishmentBean;
 import com.joooin.repository.MemberMainDao;
 import com.joooin.system.admin._03.service.LoginService;
 import com.joooin.system.admin._03.service.RegisterService;
@@ -39,14 +45,19 @@ public class LoginController {
 
 	@RequestMapping(value = "login", method = RequestMethod.POST)
 	public String loginProcess(@ModelAttribute("memberMainBean") MemberMainBean mmb, HttpServletRequest request,
-			RedirectAttributes redirectAttributes) {
+			RedirectAttributes redirectAttributes) throws ParseException {
 		HttpSession session = request.getSession();
 		MemberMainBean member = null;
+		PunishmentBean pb = null;
 		AdminBean admin = null;
 		member = service.checkEmailPassword(mmb.getEmail(), mmb.getPassword());
 		admin = service.checkAdmin(mmb.getEmail(), mmb.getPassword());
+		
+		if(member!=null) {
+			pb = service.checkPunishment(member.getMemberId());
+		}
 
-		if (member != null && member.getCertificationStatus() == true) {
+		if (member != null && member.getCertificationStatus() == true && pb == null) {
 			session.setAttribute("memberName", member.getMemberName());
 			session.setAttribute("memberId", member.getMemberId());
 			Integer logins = member.getLogins() + 1;
@@ -59,6 +70,28 @@ public class LoginController {
 		if (member != null && member.getCertificationStatus() == false) {
 			redirectAttributes.addFlashAttribute("error", member.getMemberName() + "您好，請至信箱收信驗證後才可登入");
 			return "redirect:/login";
+		}
+		
+		if (member!=null && pb!=null) {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd ahh:mm:ss");
+			Date startDate = sdf.parse(pb.getPunishDateStart());
+			Date endDate = sdf.parse(pb.getPunishDateEnd());
+			Date nowDate = new Date();
+			long startTime = startDate.getTime();
+			long endTime = endDate.getTime();
+			long nowTime = nowDate.getTime();
+			if(nowTime > startTime && nowTime < endTime) {
+				redirectAttributes.addFlashAttribute("error", "帳號鎖定中"+pb.getPunishDateEnd()+"後解鎖");
+				return "redirect:/login";
+			}else {
+				session.setAttribute("memberName", member.getMemberName());
+				session.setAttribute("memberId", member.getMemberId());
+				Integer logins = member.getLogins() + 1;
+				member.setLogins(logins);
+				service.update(member);
+				session.setAttribute("logout", "登出");
+				return "redirect:/";
+			}
 		}
 
 		if (admin != null) {
